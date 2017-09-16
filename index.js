228270c15fdb44860bb12ecd37b5b33ef78e0a68
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-09-17 00:06:49 +0200

    pushing forward

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 "use strict";
 
-const Pusher = require("pusher-js/node");
-const Bitstamp = require("./lib/Bitstamp.js");
+const Coinpusher = require("./lib/Coinpusher.js");
 
-const key = "de504dc5763aeef9ff52";
-const currency = "etheur";
-
-const socket = new Pusher(key);
-
-const liveTrades = socket.subscribe(`live_trades_${currency}`);
-liveTrades.bind("trade", data => {
-    const {amount, price, timestamp, type} = data;
-    const cost = amount * price;
-    console.log(`${currency} ${amount} ${type ? "sell" : "buy"} for ${parseInt(cost)} €, c: ${price} @ ${timestamp}.`);
-});
-
-const bitstamp = new Bitstamp();
-bitstamp.ticker(Bitstamp.CURRENCY.ETH_BTC).then(ticker => {
-    console.log(ticker.body);
-});
+const cp = new Coinpusher();
+cp.start();
